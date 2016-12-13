00a61e7aec33175e7e10d888477697d290e09380
Alex Vance <alexdvance@gmail.com>
2016-12-12 22:09:07 -0500

    Hooked up scroll to element for navigator links

--- app/components/navigator/navigator.js
+++ app/components/navigator/navigator.js
@@ -1,23 +1,37 @@
 'use strict';
 
-function NavigatorCtrl(DataModel) {
-  this.setActiveFieldGroup = DataModel.setActiveFieldGroup;
-  this.getActiveFieldGroup = DataModel.getActiveFieldGroup;
-  this.setActiveField = DataModel.setActiveField;
-  this.getActiveField = DataModel.getActiveField;
-}
+angular.module('ba.navigator', [
+  'duScroll',
+])
 
-angular.module('ba.navigator', [])
-
-.component('navigator', {
-  templateUrl: '/components/navigator/navigator.html',
-  controller: [
-    'DataModel',
-    NavigatorCtrl
-  ],
-  bindings: {
-    items: '='
-  }
+.directive('navigator', function() {
+  return {
+    templateUrl: '/components/navigator/navigator.html',
+    controllerAs: '$ctrl',
+    controller: 'NavigatorCtrl',
+    scope: {
+      items: '='
+    },
+    bindToController: true,
+    link: function($scope) {
+    }
+  };
 })
+
+.controller('NavigatorCtrl', [
+  '$scope',
+  'DataModel',
+  function($scope, DataModel) {
+    this.setActiveFieldGroup = DataModel.setActiveFieldGroup;
+    this.getActiveFieldGroup = DataModel.getActiveFieldGroup;
+    this.setActiveField = DataModel.setActiveField;
+    this.getActiveField = DataModel.getActiveField;
+
+    this.scrollToTarget = function(id) {
+      var containerEl = document.getElementById('info-section');
+      var targetEl = document.getElementById(id);
+      angular.element(containerEl).scrollToElementAnimated(targetEl, 20, 400);
+    }
+}])
 
 ;
